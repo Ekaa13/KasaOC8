363e9df8fce9bf49c6d62d0c42887514cf09cb18
Alexis Rebeyrol <aldorigen28@gmail.com>
2024-04-19 16:23:41 +0200

    organisation et reacter router

---
+++ Kasa/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Housing from "./pages/Housing";
+import Error from "./pages/Error";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+ 
+
+export default App;
